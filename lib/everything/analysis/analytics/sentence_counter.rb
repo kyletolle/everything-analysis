10379835a07c90fd55c4840180c75ef821aef9d0
Kyle Tolle <kyle@nullsix.com>
2021-10-04 13:02:56 -0600

    Only load the nlp pipeline a single time for all sentence counters

--- lib/everything/analysis/analytics/sentence_counter.rb
+++ lib/everything/analysis/analytics/sentence_counter.rb
@@ -1,12 +1,29 @@
 # frozen_string_literal: true
 
-require 'tactful_tokenizer'
 require 'stanford-core-nlp'
 
 module Everything
   class Analysis
     module Analytics
       class SentenceCounter
+        class << self
+          def pipeline
+            @pipeline ||= begin
+              StanfordCoreNLP.use :english
+              StanfordCoreNLP.model_files = {}
+              StanfordCoreNLP.default_jars = [
+                'joda-time.jar',
+                'xom.jar',
+                'stanford-corenlp-3.5.0.jar',
+                'stanford-corenlp-3.5.0-models.jar',
+                'jollyday.jar',
+                'bridge.jar'
+              ]
+              StanfordCoreNLP.load(:tokenize, :ssplit)
+            end
+          end
+        end
+
         attr_accessor :piece_markdown, :piece_title, :sentences_count
 
         def name
@@ -22,20 +39,9 @@ module Everything
           # Following steps at
           # https://github.com/louismullie/stanford-core-nlp#using-the-latest-version-of-the-stanford-corenlp
           # helped get me get the stanford corenlp gem working!
-          StanfordCoreNLP.use :english
-          StanfordCoreNLP.model_files = {}
-          StanfordCoreNLP.default_jars = [
-            'joda-time.jar',
-            'xom.jar',
-            'stanford-corenlp-3.5.0.jar',
-            'stanford-corenlp-3.5.0-models.jar',
-            'jollyday.jar',
-            'bridge.jar'
-          ]
-
-          pipeline = StanfordCoreNLP.load(:tokenize, :ssplit)
+          self.class.pipeline
           text = StanfordCoreNLP::Annotation.new(piece_markdown)
-          pipeline.annotate(text)
+          self.class.pipeline.annotate(text)
           self.sentences_count = text.get(:sentences).size
 
           self
