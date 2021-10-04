9b572e42d34aeb7ebf6c65494af9d5c9bed3cebb
Kyle Tolle <kyle@nullsix.com>
2021-10-04 14:57:56 -0600

    Display sentiment categories too

--- lib/everything/analysis/analytics/sentiment_analyzer.rb
+++ lib/everything/analysis/analytics/sentiment_analyzer.rb
@@ -5,7 +5,7 @@ module Everything
   class Analysis
     module Analytics
       class SentimentAnalyzer
-        attr_accessor :piece_markdown, :piece_title, :sentiments
+        attr_accessor :piece_markdown, :piece_title, :sentiments, :sentiment_categories
 
         def name
           'Sentiment Analyzer'
@@ -25,15 +25,36 @@ module Everything
         def run
           @run_result ||= begin
             self.sentiments = {}
+            self.sentiment_categories = {
+              negative: 0,
+              neutral: 0,
+              positive: 0,
+            }
             # See https://github.com/nzaillian/sentiment_lib#usage for how to
             # use the sentiment analyzer
-            analyzer = SentimentLib::Analyzer.new
+            analyzer = SentimentLib::Analyzer.new(strategy: SentimentLib::Analysis::Strategies::BasicDictStrategy.new)
 
             sentences.each do |sentence|
               sentiment = analyzer.analyze(sentence.to_s)
-              sentiment = sentiment.to_f
+              sentiment = sentiment.to_f.ceil(0)
               sentiments[sentiment] ||= 0
               sentiments[sentiment] += 1
+            end
+
+            self.sentiments = sentiments
+              .sort{ |a, b| a <=> b }
+
+            sentiments.each do |sentiment, count|
+              category = case sentiment
+                when -Float::INFINITY..-1
+                  :negative
+                when 0
+                  :neutral
+                when 1..Float::INFINITY
+                  :positive
+              end
+              self.sentiment_categories[category] ||= 0
+              self.sentiment_categories[category] += count
             end
           end
 
@@ -41,12 +62,25 @@ module Everything
         end
 
         def to_s
+          total_sentences = sentences.size
+
           sentiments_string = sentiments
-            .map { |k, v| "      #{v} sentences had the sentiment rating of #{k}" }
+            .map do |sentiment_rating, count|
+              percent = ((count.to_f / total_sentences)*100).ceil(2)
+              "      #{count} sentences had the sentiment rating of #{sentiment_rating} (#{percent}%) "
+            end
+            .join("\n")
+
+          category_counts = sentiment_categories
+            .map do |category, count|
+              percent = ((count.to_f / total_sentences)*100).ceil(2)
+              "      #{count} sentences were #{category} (#{percent}%)"
+            end
             .join("\n")
 
           "  #{name}\n" \
-          "    Sentiments: \n#{sentiments_string}"
+          "    Sentiments: \n#{sentiments_string}\n\n" \
+          "    Sentiment Categories: \n#{category_counts}"
         end
       end
     end
