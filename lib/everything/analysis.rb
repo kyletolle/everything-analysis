35dca5e144f996e04fbcdab4e958f9075a953946
Kyle Tolle <kyle@nullsix.com>
2021-10-06 11:52:06 -0600

    Add some more ideas for additional analytics

--- lib/everything/analysis.rb
+++ lib/everything/analysis.rb
@@ -98,6 +98,17 @@ module Everything
         Everything::Analysis::Analytics::SentimentAnalyzer
       ]
       # What other analytics?
+      # Calculate Flesch Reading Ease or Flesch-Kincaid Grade Levels for each chapter and the book as a whole?
+      # Calculate time to read for each chapter and the book as a whole?
+      #   - Could use an approach like https://niram.org/read/ and assume 200 WPM is avg reading speed.
+      #   - Also want to read https://w3collective.com/calculate-reading-time-javascript/
+      #   - Then read https://cs.stackexchange.com/questions/57285/how-to-calculate-an-accurate-estimated-reading-time-of-text
+      #   - And https://stackoverflow.com/questions/4216752/anyone-having-any-leads-on-a-reading-time-algorithm
+      #   - And then it'd be nice to round the time from 3min 32 seconds to 3 and a half minutes using an approach like https://stackoverflow.com/questions/3278986/ago-date-time-functions-in-ruby-rails
+      # Look at other nlp tools at
+      #  - https://github.com/diasks2/ruby-nlp#word-count
+      #  - https://github.com/arbox/nlp-with-ruby
+      #  - https://github.com/louismullie/treat
       # Compare character frequency % for myself to the global %s to see what
       # characters I use more than other writers. Idea from Zach
       # Most common words per chapter - also from Zach
