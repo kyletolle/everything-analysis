0b4e69d0d0dd71203ab9b53f7df7609571fb261a
Kyle Tolle <kyle@nullsix.com>
2021-10-06 11:51:55 -0600

    Remove table cli command that was just for testing

--- lib/everything/analysis/cli.rb
+++ lib/everything/analysis/cli.rb
@@ -8,14 +8,6 @@ module Everything
         analysis.run_and_print
       end
 
-      desc 'table', 'display a test for the new table output'
-      def table
-        new_table = Everything::Analysis::Table.new
-        new_table.add_columns :character, :usage
-        new_table.add_row({ character: 'a', usage: 50.to_s })
-        puts new_table.to_s
-      end
-
       private
 
       def analysis
