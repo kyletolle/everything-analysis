d066179eeffd4e52216548bda2429948eb932b8d
Kyle Tolle <kyle@nullsix.com>
2021-10-06 11:51:42 -0600

    Make the table output use characters that make it look like a proper box

--- lib/everything/analysis/table.rb
+++ lib/everything/analysis/table.rb
@@ -3,6 +3,18 @@ require 'active_support/core_ext/string/inflections'
 module Everything
   class Analysis
     class Table
+      BOX_UPPER_LEFT = '┏'
+      BOX_UPPER_INTERSECTION = '┳'
+      BOX_UPPER_RIGHT = '┓'
+      BOX_MIDDLE_LEFT = '┣'
+      BOX_MIDDLE_INTERSECTION = '╋'
+      BOX_MIDDLE_RIGHT = '┫'
+      BOX_LOWER_LEFT = '┗'
+      BOX_LOWER_INTERSECTION = '┻'
+      BOX_LOWER_RIGHT = '┛'
+      BOX_HORIZONTAL_DASH = '━'
+      BOX_VERTICAL_DASH = '┃'
+
       attr_accessor :columns, :rows, :spaces_to_pad_at_beginning_of_each_line
 
       def initialize(spaces_to_pad_at_beginning_of_each_line:)
@@ -35,23 +47,36 @@ module Everything
           column[:name].to_s.pluralize.capitalize.ljust(column[:max_length])
         end
 
+
         line_padding = ' ' * spaces_to_pad_at_beginning_of_each_line
 
         justified_texts = self.rows.map do |row|
-          "#{line_padding}| " + row.map.with_index do |row_value, index|
+          "#{line_padding}#{BOX_VERTICAL_DASH} " + row.map.with_index do |row_value, index|
             column = columns[index]
             row_value.ljust(column[:max_length])
-          end.join(' | ') + ' |'
+          end.join(" #{BOX_VERTICAL_DASH} ") + " #{BOX_VERTICAL_DASH}"
         end
 
-        header_text = "| #{headers.join(' | ')} |\n"
-        spacer_line = "#{line_padding}|#{'-' * (header_text.length - 3)}|\n"
+        header_text = "#{BOX_VERTICAL_DASH} #{headers.join(" #{BOX_VERTICAL_DASH} ")} #{BOX_VERTICAL_DASH}\n"
+        # box_horizontal_line = BOX_HORIZONTAL_DASH * (header_text.length - 3)
+        box_top_line = self.columns.map do |column|
+          BOX_HORIZONTAL_DASH * (column[:max_length] + 2)
+        end.join(BOX_UPPER_INTERSECTION)
+        top_line_spacer = "#{line_padding}#{BOX_UPPER_LEFT}#{box_top_line}#{BOX_UPPER_RIGHT}\n"
+        box_middle_line = self.columns.map do |column|
+          BOX_HORIZONTAL_DASH * (column[:max_length] + 2)
+        end.join(BOX_MIDDLE_INTERSECTION)
+        middle_line_spacer = "#{line_padding}#{BOX_MIDDLE_LEFT}#{box_middle_line}#{BOX_MIDDLE_RIGHT}\n"
+        box_lower_line = self.columns.map do |column|
+          BOX_HORIZONTAL_DASH * (column[:max_length] + 2)
+        end.join(BOX_LOWER_INTERSECTION)
+        bottom_line_spacer = "#{line_padding}#{BOX_LOWER_LEFT}#{box_lower_line}#{BOX_LOWER_RIGHT}\n"
 
-        "#{spacer_line}" \
+        "#{top_line_spacer}" \
         "#{line_padding}#{header_text}" \
-        "#{spacer_line}" \
+        "#{middle_line_spacer}" \
         "#{justified_texts.join("\n")}\n" \
-        "#{spacer_line}\n"
+        "#{bottom_line_spacer}\n"
       end
     end
   end
