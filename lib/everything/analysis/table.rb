1028a525aea7f6ffe1f7873098e51941cffd46ec
Kyle Tolle <kyle@nullsix.com>
2021-10-06 11:55:47 -0600

    Make some small tweaks
    Move a join into the justified texts and rename some variables

--- lib/everything/analysis/table.rb
+++ lib/everything/analysis/table.rb
@@ -55,28 +55,28 @@ module Everything
             column = columns[index]
             row_value.ljust(column[:max_length])
           end.join(" #{BOX_VERTICAL_DASH} ") + " #{BOX_VERTICAL_DASH}"
-        end
+        end.join("\n")
 
         header_text = "#{BOX_VERTICAL_DASH} #{headers.join(" #{BOX_VERTICAL_DASH} ")} #{BOX_VERTICAL_DASH}\n"
         # box_horizontal_line = BOX_HORIZONTAL_DASH * (header_text.length - 3)
-        box_top_line = self.columns.map do |column|
+        inner_upper_line = self.columns.map do |column|
           BOX_HORIZONTAL_DASH * (column[:max_length] + 2)
         end.join(BOX_UPPER_INTERSECTION)
-        top_line_spacer = "#{line_padding}#{BOX_UPPER_LEFT}#{box_top_line}#{BOX_UPPER_RIGHT}\n"
-        box_middle_line = self.columns.map do |column|
+        entire_upper_line = "#{line_padding}#{BOX_UPPER_LEFT}#{inner_upper_line}#{BOX_UPPER_RIGHT}\n"
+        inner_middle_line = self.columns.map do |column|
           BOX_HORIZONTAL_DASH * (column[:max_length] + 2)
         end.join(BOX_MIDDLE_INTERSECTION)
-        middle_line_spacer = "#{line_padding}#{BOX_MIDDLE_LEFT}#{box_middle_line}#{BOX_MIDDLE_RIGHT}\n"
-        box_lower_line = self.columns.map do |column|
+        entire_middle_line = "#{line_padding}#{BOX_MIDDLE_LEFT}#{inner_middle_line}#{BOX_MIDDLE_RIGHT}\n"
+        inner_lower_line = self.columns.map do |column|
           BOX_HORIZONTAL_DASH * (column[:max_length] + 2)
         end.join(BOX_LOWER_INTERSECTION)
-        bottom_line_spacer = "#{line_padding}#{BOX_LOWER_LEFT}#{box_lower_line}#{BOX_LOWER_RIGHT}\n"
+        entire_lower_line = "#{line_padding}#{BOX_LOWER_LEFT}#{inner_lower_line}#{BOX_LOWER_RIGHT}\n"
 
-        "#{top_line_spacer}" \
+        "#{entire_upper_line}" \
         "#{line_padding}#{header_text}" \
-        "#{middle_line_spacer}" \
-        "#{justified_texts.join("\n")}\n" \
-        "#{bottom_line_spacer}\n"
+        "#{entire_middle_line}" \
+        "#{justified_texts}\n" \
+        "#{entire_lower_line}\n"
       end
     end
   end
