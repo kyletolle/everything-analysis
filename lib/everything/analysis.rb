89b76457ac0312c7f096a489d6af40b9fec824d3
Kyle Tolle <kyle@nullsix.com>
2021-10-09 16:36:59 -0600

    Extract a constant for Analytics::TO_RUN

--- lib/everything/analysis.rb
+++ lib/everything/analysis.rb
@@ -33,7 +33,7 @@ module Everything
       @completed_analytics ||= pieces_data_to_analyze.map do |piece_data|
         piece_title = piece_data[:piece_title]
         piece_markdown = piece_data[:piece_markdown]
-        analytics = analysis_to_perform.map do |analysis_klass|
+        analytics = Analytics::TO_RUN.map do |analysis_klass|
           analysis_klass.new(piece_title: piece_title, piece_markdown: piece_markdown).run
         end
         { piece_title: piece_title, analytics: analytics }
@@ -64,7 +64,7 @@ module Everything
     def completed_analytics_for_all
       piece_title = all_pieces_data[:piece_title]
       piece_markdown = all_pieces_data[:piece_markdown]
-      analytics = analysis_to_perform.map do |analysis_klass|
+      analytics = Analytics::TO_RUN.map do |analysis_klass|
         analysis_klass.new(piece_title: piece_title, piece_markdown: piece_markdown).run
       end
       { piece_title: piece_title, analytics: analytics }
@@ -93,38 +93,6 @@ module Everything
           Pathname.new(pieces_path).join(path)
         end
       end.flatten
-    end
-
-    def analysis_to_perform
-      [
-        Everything::Analysis::Analytics::CharacterFrequency,
-        Everything::Analysis::Analytics::EstimatedReadingTime,
-        Everything::Analysis::Analytics::ParagraphCounter,
-        Everything::Analysis::Analytics::PunctuationPercentage,
-        Everything::Analysis::Analytics::SentenceCounter,
-        Everything::Analysis::Analytics::SentimentAnalyzer,
-        Everything::Analysis::Analytics::WhitespacePercentage,
-        Everything::Analysis::Analytics::WordCounter,
-        Everything::Analysis::Analytics::WordFrequency,
-      ]
-      # What other analytics?
-      # Calculate Flesch Reading Ease or Flesch-Kincaid Grade Levels for each chapter and the book as a whole?
-      # Calculate time to read for each chapter and the book as a whole?
-      #   - Could use an approach like https://niram.org/read/ and assume 200 WPM is avg reading speed.
-      #   - Also want to read https://w3collective.com/calculate-reading-time-javascript/
-      #   - Then read https://cs.stackexchange.com/questions/57285/how-to-calculate-an-accurate-estimated-reading-time-of-text
-      #   - And https://stackoverflow.com/questions/4216752/anyone-having-any-leads-on-a-reading-time-algorithm
-      #   - And then it'd be nice to round the time from 3min 32 seconds to 3 and a half minutes using an approach like https://stackoverflow.com/questions/3278986/ago-date-time-functions-in-ruby-rails
-      # Look at other nlp tools at
-      #  - https://github.com/diasks2/ruby-nlp#word-count
-      #  - https://github.com/arbox/nlp-with-ruby
-      #  - https://github.com/louismullie/treat
-      # Lemmatize all the words and see what number of unique root are used, and their frequency too.
-      # Compare character frequency % for myself to the global %s to see what
-      # characters I use more than other writers. Idea from Zach
-      # Most common words per chapter - also from Zach
-      # He mentions the plot he made from
-      # https://github.com/ZWMiller/PythonProjects/blob/master/wordCounter/img/toaesWordCountPlot.png
     end
   end
 end
--- lib/everything/analysis/analytics.rb
+++ lib/everything/analysis/analytics.rb
@@ -1,12 +1,5 @@
 # frozen_string_literal: true
 
-module Everything
-  class Analysis
-    module Analytics
-    end
-  end
-end
-
 require_relative './analytics/analytic_base'
 require_relative './analytics/character_frequency'
 require_relative './analytics/estimated_reading_time'
@@ -17,3 +10,39 @@ require_relative './analytics/sentiment_analyzer'
 require_relative './analytics/whitespace_percentage'
 require_relative './analytics/word_frequency'
 require_relative './analytics/word_counter'
+
+module Everything
+  class Analysis
+    module Analytics
+      # What other analytics?
+      # Calculate Flesch Reading Ease or Flesch-Kincaid Grade Levels for each chapter and the book as a whole?
+      # Calculate time to read for each chapter and the book as a whole?
+      #   - Could use an approach like https://niram.org/read/ and assume 200 WPM is avg reading speed.
+      #   - Also want to read https://w3collective.com/calculate-reading-time-javascript/
+      #   - Then read https://cs.stackexchange.com/questions/57285/how-to-calculate-an-accurate-estimated-reading-time-of-text
+      #   - And https://stackoverflow.com/questions/4216752/anyone-having-any-leads-on-a-reading-time-algorithm
+      #   - And then it'd be nice to round the time from 3min 32 seconds to 3 and a half minutes using an approach like https://stackoverflow.com/questions/3278986/ago-date-time-functions-in-ruby-rails
+      # Look at other nlp tools at
+      #  - https://github.com/diasks2/ruby-nlp#word-count
+      #  - https://github.com/arbox/nlp-with-ruby
+      #  - https://github.com/louismullie/treat
+      # Lemmatize all the words and see what number of unique root are used, and their frequency too.
+      # Compare character frequency % for myself to the global %s to see what
+      # characters I use more than other writers. Idea from Zach
+      # Most common words per chapter - also from Zach
+      # He mentions the plot he made from
+      # https://github.com/ZWMiller/PythonProjects/blob/master/wordCounter/img/toaesWordCountPlot.png
+      TO_RUN = [
+        CharacterFrequency,
+        EstimatedReadingTime,
+        ParagraphCounter,
+        PunctuationPercentage,
+        SentenceCounter,
+        SentimentAnalyzer,
+        WhitespacePercentage,
+        WordCounter,
+        WordFrequency,
+      ]
+    end
+  end
+end
